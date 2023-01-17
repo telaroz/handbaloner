d99b5b020edb226b374ef4dfaaa41df86301d3c0
telaroz <telaroz@gmail.com>
2023-01-17 12:13:52 -0600

    Add utilities for ploting pace data.

--- R/data.R
+++ R/data.R
@@ -15,3 +15,36 @@ usethis::use_data(xg_egipto21, xg_egipto21, overwrite = TRUE)
 #' }
 #' @source \url{'https://raw.githubusercontent.com/telaroz/egipto21/main/partidos_pbp_egipto2021.csv'}
 "xg_egipto21"
+
+
+#' Complete names
+#'
+#'
+#' @format A data frame with 32 rows and 2 variables:
+#' \describe{
+#'   \item{team}{Short Name}
+#'   \item{complete_name}{Complete Name}
+#'   ...
+#' }
+"complete_team_names"
+complete_team_names <-
+  data.table::data.table(team = c("FRA", "POL", "CHI", "IRI", "ESP", "MNE",
+                                  "KSA", "SLO", "CPV", "URU", "SWE", "BRA",
+                                  "HUN", "KOR", "ISL","POR", "ARG", "NED",
+                                  "NOR", "MKD", "SRB", "ALG", "GER", "QAT",
+                                  "BRN", "TUN", "DEN", "BEL", "MAR", "USA",
+                                  "EGY", "CRO"),
+                         complete_name = c("France", "Poland", "Chile", "Iran",
+                                           "Spain", "Montenegro", "Saudi Arabia",
+                                           "Slovenia", "Cape Verde", "Uruguay",
+                                           "Sweden", "Brazil", "Hungary",
+                                           "South Korea", "Iceland","Portugal",
+                                           "Argentina", "Netherlands", "Norway",
+                                           "North Macedonia", "Serbia",
+                                           "Algeria", "Germany", "Qatar",
+                                           "Bahrain", "Tunisia", "Denmark",
+                                           "Belgium", "Morocco",
+                                           "United States of America", "Egypt",
+                                           "Croatia"))
+
+usethis::use_data(complete_team_names, complete_team_names, overwrite = TRUE)
--- R/utilidades.R
+++ R/utilidades.R
@@ -92,3 +92,57 @@ scrape_from_ihf <- function(link, folder, from_archive = FALSE){
                              stringr::str_remove('[?=].*')),
                  download.file, mode = "wb")
 }
+
+#' Assign a band to the values of a numerical column
+#'
+#' @param data Table to add band
+#' @param minutes_column Name of the numerical column.
+#' @param band_cuts Numerical vector specifying the cuts of the bands.
+#' @param bands_column_name Name of the column to be created.
+#' @param number_bands Whether or not to add numbering to the bands.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'\dontrun{
+#' add_bands(data, "minutes",
+#' c(0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60))
+#'}
+add_bands <- function(data,
+                      minutes_column,
+                      band_cuts = c(0, 1, 5, 10, 25, 50, 100, 150, 200, 300, 500, 750, 1000, 1500, 2000),
+                      bands_column_name = 'minutes_band',
+                      truncate_in_data_max = FALSE,
+                      number_bands = FALSE) {
+
+  if(!(minutes_column %chin% colnames(data))){
+    stop('Column ', minutes_column, ' not in data. ')
+  }
+
+  # create empty column
+  data.table::setDT(data)
+  data[, (bands_column_name) := NA_character_]
+
+  # index for the bands
+  run <- seq_along(band_cuts)
+  if(truncate_in_data_max){
+    band_cuts <- c(band_cuts, max(data[[minutes_column]], na.rm = TRUE) + 1) %>% dq::suniq()
+  }else{
+    band_cuts <- band_cuts %>% dq::suniq()
+  }
+  band_length <- length(band_cuts)
+
+  # Nice names for bands
+  band_names <- paste0(if(number_bands){paste0(fifelse(1:(band_length-1) > 9, '', '0'), run, '. ')},
+                       '[',band_cuts[-band_length], '-', band_cuts[-1], ']')
+
+  # assign band to each value
+  purrr::walk(run, ~ data.table::set(data,
+                                     which(band_cuts[.x] <= data[[minutes_column]] & data[[minutes_column]] < band_cuts[.x + 1]),
+                                     bands_column_name,
+                                     band_names[.x])[])
+
+  data[, (bands_column_name) := factor(x = get(bands_column_name), levels = band_names)]
+}
+
